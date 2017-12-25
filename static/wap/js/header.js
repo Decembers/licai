bf08ee82036fab6b2ec6b7cbb170d49aa6dbd36a
赵 <13203940281@163.com>
2017-12-25 16:45:05 +0800

    header.js

--- static/wap/js/header.js
+++ static/wap/js/header.js
@@ -1,39 +1,41 @@
-// /*有.footer 时 .wrapper 下面的外边距 */
-// $(".wrapper").css("padding-bottom", $(".footer").height() + "px");
+/*有.footer 时 .wrapper 下面的外边距 */
+$(".wrapper").css("padding-bottom", $(".footer").height() + "px");
 
-// /*有.footer 时他与 .wrapper 的宽等宽 */
-// $(".header").css("width", $(".wrapper").width() + "px");
-// /*有.header 时 .wrapper 上面的外边距 */
-// $(".wrapper").css("padding-top", ($(".header").height()) + "px");
-// /*$(".packet").css("width", $(".wrapper").width() + "px");*/
+/*有.footer 时他与 .wrapper 的宽等宽 */
+$(".header").css("width", $(".wrapper").width() + "px");
+/*有.header 时 .wrapper 上面的外边距 */
+$(".wrapper").css("padding-top", ($(".header").height()) + "px");
+/*$(".packet").css("width", $(".wrapper").width() + "px");*/
 
+$(".overtop").css("border-radius","1.5rem");
+$(".overtop").css("overflow","hidden");
 
+/*禁止右键 点击  复制*/
+/*$(document).bind("contextmenu copy selectstart", function() {
+	return false;
+});
+$(document).keydown(function(e) {
+	if(e.ctrlKey && (e.keyCode == 65 || e.keyCode == 67)) {
+		return false;
+	}
+})*/
 
-// /*禁止右键 点击  复制*/
-// $(document).bind("contextmenu copy selectstart", function() {
-// 	return false;
-// });
-// $(document).keydown(function(e) {
-// 	if(e.ctrlKey && (e.keyCode == 65 || e.keyCode == 67)) {
-// 		return false;
-// 	}
-// })
-// /**/
-// if($("body").width() > 900) {
-// 	$(".wrapper").css("left", ($("body").width() - 800) / 2 + "px");
-// }
-// $("div.header>div").click(function() {
-// 	history.go(-1)
-// //	history.back(-1)
-// })
-// /*提示*/
-// function overtop() {
-// 				$(".detail").after($("body>p.overtop").clone(true));
-// 				var ss1 = $(".wrapper .overtop");
-// 				ss1.css("display", "block");
-// 				ss1.css("opacity", "1");
-// 				setTimeout(function() {
-// 					ss1.css("opacity", "0");
-// 					ss1.remove();
-// 				}, 1000);
-// 			}
+if($("body").width() > 900) {
+	$(".wrapper").css("left", ($("body").width() - 800) / 2 + "px");
+}
+$("div.header>div").click(function() {
+	history.go(-1)
+/*history.back(-1)*/
+})
+/*提示*/
+function overtop() {
+				$(".wrapper>div:nth-child(1)").after($("body>p.overtop").clone(true));
+				var ss1 = $(".wrapper .overtop");
+				$(".wrapper .overtop").css("background-color","rgba(0,0,0,0.7)");
+				ss1.css("display", "block");
+				ss1.css("opacity", "1");
+				setTimeout(function() {
+					ss1.css("opacity", "0");
+					ss1.remove();
+				}, 1000);
+			}
