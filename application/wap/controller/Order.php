a85d5c179bf7992c05b1d8c0bd6e18abff374c0d
licai <php_programmer@163.com>
2018-01-12 16:45:05 +0800

    算法

--- application/wap/controller/Order.php
+++ application/wap/controller/Order.php
@@ -3,7 +3,7 @@
  * @Author: Marte
  * @Date:   2017-12-12 17:12:51
  * @Last Modified by:   Marte
- * @Last Modified time: 2018-01-12 16:20:26
+ * @Last Modified time: 2018-01-12 16:28:39
  */
 namespace app\wap\controller;
 use app\wap\controller\Yang;
@@ -123,7 +123,7 @@ class Order extends Yang
                 if (isset($orderss)) {
                     $update['order_price'] = $orderss['order_price']+$order_price;
                     $update['sp_count'] = $orderss['sp_count']+$num;
-                    $update['zexpect'] = substr(sprintf("%.3f",$update['sp_count']*$orderss['sp_price']*$comm['expect']*$comm['nexpect']),0,-1);
+                    $update['zexpect'] = substr(sprintf("%.3f",$update['sp_count']*$comm['expect']*$comm['nexpect']),0,-1);
                     $ara = Db::table('tp_order')->where(['user_id'=>$this->id,'sp_id'=>$sp_id])->update($update);
                 }else{
                     $ara = Db::table('tp_order')->insert($row);
