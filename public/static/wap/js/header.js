5220bac6389949fe8ac7aabc93b02bf34308a14f
赵 <13203940281@163.com>
2018-01-16 14:12:33 +0800

    1213

--- public/static/wap/js/header.js
+++ public/static/wap/js/header.js
@@ -26,13 +26,13 @@ function overtop() {
 			}
 function overtop2() {
 				$(".wrapper>div:nth-child(1)").after($("body>p.overtop").clone(true));
-				var ss1 = $(".wrapper .overtop");
+				var ss2 = $(".wrapper .overtop");
 				$(".wrapper .overtop").css("background-color","rgba(0,0,0,0.7)");
-				ss1.css("display", "block");
-				ss1.css("opacity", "1");
+				ss2.css("display", "block");
+				ss2.css("opacity", "1");
 				setTimeout(function() {
-					ss1.css("opacity", "0");
-					ss1.remove();
+					ss2.css("opacity", "0");
+					ss2.remove();
 				}, 2000);  
 			}
 
