971acafa8c8632024c7aba127daca070b256a04e
licai <php_programmer@163.com>
2018-02-08 10:20:33 +0800

    aa

---
+++ application/admin/controller/UserPacket.php
@@ -0,0 +1,67 @@
+<?php
+namespace app\admin\controller;
+
+\think\Loader::import('controller/Controller', \think\Config::get('traits_path') , EXT);
+
+use app\admin\Controller;
+
+class UserPacket extends Controller
+{
+    use \app\admin\traits\controller\Controller;
+    // 方法黑名单
+    protected static $blacklist = [];
+    /**
+     * 首页
+     * @return mixed
+     */
+    public function register()
+    {
+
+        $model = $this->getModel();
+
+        // 列表过滤器，生成查询Map对象
+        $map = $this->search($model, [$this->fieldIsDelete => $this::$isdelete,'id'=>1]);
+
+        // 特殊过滤器，后缀是方法名的
+        $actionFilter = 'filter' . $this->request->action();
+        if (method_exists($this, $actionFilter)) {
+            $this->$actionFilter($map);
+        }
+
+        // 自定义过滤器
+        if (method_exists($this, 'filter')) {
+            $this->filter($map);
+        }
+
+        $this->datalist($model, $map);
+
+        return $this->view->fetch('index');
+    }
+    /**
+     * 首页
+     * @return mixed
+     */
+    public function index()
+    {
+
+        $model = $this->getModel();
+
+        // 列表过滤器，生成查询Map对象
+        $map = $this->search($model, [$this->fieldIsDelete => $this::$isdelete,'id'=>['neq',1]]);
+
+        // 特殊过滤器，后缀是方法名的
+        $actionFilter = 'filter' . $this->request->action();
+        if (method_exists($this, $actionFilter)) {
+            $this->$actionFilter($map);
+        }
+
+        // 自定义过滤器
+        if (method_exists($this, 'filter')) {
+            $this->filter($map);
+        }
+
+        $this->datalist($model, $map);
+
+        return $this->view->fetch('index');
+    }
+}
