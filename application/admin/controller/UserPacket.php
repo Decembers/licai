72410a8a596cc70d388ff7a8d41b2cc5e26f2cd5
licai <php_programmer@163.com>
2018-02-11 18:05:03 +0800

    aaa

--- application/admin/controller/UserPacket.php
+++ application/admin/controller/UserPacket.php
@@ -37,6 +37,8 @@ class UserPacket extends Controller
 
         return $this->view->fetch('index');
     }
+
+
     /**
      * 邀请红包模版
      * @return mixed
@@ -48,6 +50,34 @@ class UserPacket extends Controller
 
         // 列表过滤器，生成查询Map对象
         $map = $this->search($model, [$this->fieldIsDelete => $this::$isdelete,'id'=>2]);
+
+        // 特殊过滤器，后缀是方法名的
+        $actionFilter = 'filter' . $this->request->action();
+        if (method_exists($this, $actionFilter)) {
+            $this->$actionFilter($map);
+        }
+
+        // 自定义过滤器
+        if (method_exists($this, 'filter')) {
+            $this->filter($map);
+        }
+
+        $this->datalist($model, $map);
+
+        return $this->view->fetch('index');
+    }
+
+    /**
+     * 邀请红包模版
+     * @return mixed
+     */
+    public function qiandao()
+    {
+
+        $model = $this->getModel();
+
+        // 列表过滤器，生成查询Map对象
+        $map = $this->search($model, [$this->fieldIsDelete => $this::$isdelete,'id'=>['in','3,4,5']]);
 
         // 特殊过滤器，后缀是方法名的
         $actionFilter = 'filter' . $this->request->action();
