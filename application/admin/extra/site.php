c77e6dde0d81008a4b183306d89d9be13efdb6c7
licai <php_programmer@163.com>
2018-05-08 09:47:40 +0800

    aaa

--- application/admin/extra/site.php
+++ application/admin/extra/site.php
@@ -10,11 +10,11 @@
 
 return [
     // 网站信息
-    'name'        => '养羊',
-    'title'       => '养羊',
-    'version'     => 'v1.3',
-    'keywords'    => 'tpadmin,thinkphp5,thinkphp,权限管理,RBAC,后台管理系统,网站后台模版,后台模版下载,HTML后台模版下载',
-    'description' => '一个基于ThinkPHP5的管理后台，支持代码自动生成，RBAC权限管理，无限级节点权限管理，一个智能化的管理后台。',
+    'name'        => '趣味农场',
+    'title'       => '趣味农场',
+    'version'     => 'v1.0',
+    'keywords'    => '趣味农场后台管理系统',
+    'description' => '趣味农场后台管理系统',
 
     // 登录地址
     'logout_url'        => 'Pub/logout',
